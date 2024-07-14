62a5f2ce4a7b6f71271983a9781b1df83dd9d9f0
Sam Fareez <vsefareez@gmail.com>
2024-07-14 19:45:09 +0800

    refactor: remove unnecessary parameter for posting story and change size

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pylast
 from dotenv import load_dotenv
 import os
 import requests
-from imagedominantcolour import DominantColour
 
 load_dotenv()
 
@@ -12,6 +11,7 @@ LAST_FM_API_SECRET = os.getenv('LAST_FM_API_SECRET')
 WHAPI_API_URL = os.getenv('WHAPI_API_URL')
 WHAPI_TOKEN = os.getenv('WHAPI_TOKEN')
 USERNAME = 'zeerafle'
+SIZE = 770
 
 print('Connecting to Last.fm...')
 network = pylast.LastFMNetwork(api_key=LAST_FM_API_KEY, api_secret=LAST_FM_API_SECRET)
@@ -23,23 +23,11 @@ track_title = top_track.item.title
 artist_name = top_track.item.artist.name
 album = top_track.item.get_album()
 album_name = album.get_title()
-album_cover_url_small = album.get_cover_image(size=pylast.SIZE_SMALL)
-album_cover_url_medium = album.get_cover_image(size=pylast.SIZE_MEDIUM)
+album_cover_url = album.get_cover_image(size=pylast.SIZE_EXTRA_LARGE)
+album_cover_url = album_cover_url.replace('300x300', f'{SIZE}x{SIZE}')
 
 print(f"Top track of the week: {track_title} by {artist_name} from the album {album_name}")
-
-print('Downloading album cover...')
-os.makedirs(TEMP_IMAGES_PATH, exist_ok=True)
-response = requests.get(album_cover_url_small)
-with open(os.path.join(TEMP_IMAGES_PATH, 'album_cover.jpg'), 'wb') as file:
-    file.write(response.content)
-print('Album cover downloaded as album_cover.jpg')
-
-print('Getting dominant color of album cover...')
-dominant_color = DominantColour(os.path.join(TEMP_IMAGES_PATH, 'album_cover.jpg'))
-rgb_color = dominant_color.rgb
-hex_color = '#%02x%02x%02x' % rgb_color
-print(f'Dominant color of album cover: {hex_color}')
+print(f"Album cover: {album_cover_url}")
 
 print('Getting Whatsapp contact...')
 headers = {
@@ -53,12 +41,11 @@ contacts = [contact['id'] for contact in contacts['contacts']]
 
 print('Updating Whatsapp status')
 payload = {
-    "background_color": hex_color,
-    "caption_color": "#FFFFFFFF",
-    "media": album_cover_url_medium,
-    "contacts": contacts,
-    "font_type": "CALISTOGA_REGULAR",
-    "caption": "Currently obsessed with " + track_title + " by " + artist_name + " from the album " + album_name
+    "media": album_cover_url,
+    "contacts": ['62895704806187'],
+    "caption": "Currently obsessed with " + track_title + " by " + artist_name + " from the album " + album_name,
+    "width": 770,
+    "height": 770
 }
 headers = {
     "accept": "application/json",
